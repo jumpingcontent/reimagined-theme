727a5ecc4f5ac9f02b5d9103fbde246f17ad8bc8
test <test@test.com>
2018-01-31 19:17:20 -0600

    README update and Chapter 3 script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # reimagined-theme
+
+Here's just a readme for my Git project today at school
---
+++ ch3.sh
@@ -0,0 +1,11 @@
+# Chapter 3 class script example
+
+echo Testing > /dev/null
+
+ls -l
+
+dd if=file1 of=file2 bs=k count=1
+
+udevadm info --query=all --name=/dev/sda
+
+udevadm monitor
